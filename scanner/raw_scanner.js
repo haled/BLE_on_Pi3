96fbadca6c7bbfd9ab677ae0ac066d2e67d85464
Darren Hale <darrenhale@darrenhale.clearent.lan>
2016-06-24 22:30:16 -0500

    FIxed it. k# Please enter the commit message for your changes. Lines starting

--- scanner/raw_scanner.js
+++ scanner/raw_scanner.js
@@ -1,11 +1,20 @@
-var beaconScanner = require('my-eddystone-beacon-scanner');
+var beaconScanner = require('eddystone-beacon-scanner');
+
+function showMyBeacon(beacon, id) {
+  if(beacon.id === id) {
+    console.log('Got my beacon:\n', JSON.stringify(beacon, null, 2));
+    //beaconScanner.stopScanning();
+  }
+}
 
 beaconScanner.on('found', function(beacon) {
   console.log('found Eddystone Beacon:\n', JSON.stringify(beacon, null, 2));
+  //showMyBeacon(beacon, '3277a7fc917441f8aa5b23f7878b84d9');
 });
 
 beaconScanner.on('updated', function(beacon) {
-  console.log('updated Eddystone Beacon:\n', JSON.stringify(beacon, null, 2));
+  //console.log('updated Eddystone Beacon:\n', JSON.stringify(beacon, null, 2));
+  showMyBeacon(beacon, '3277a7fc917441f8aa5b23f7878b84d9');
 });
 
 beaconScanner.on('lost', function(beacon) {
